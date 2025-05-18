c25b6c870de4fd02248146edb4ed5d9c5e2ee133
flutterbea <loveebea7@gmail.com>
2025-05-18 15:16:49 +0000

    )

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function showSection(id) {
-  const contents = document.querySelectorAll('.content');
-  contents.forEach(c => c.classList.add('hidden'));
-
-  document.getElementById(id).classList.remove('hidden');
+  const sections = document.querySelectorAll('.content');
+  sections.forEach(s => s.classList.remove('active'));
+  document.getElementById(id).classList.add('active');
 }
+
+// Открыть первую секцию по умолчанию
+document.addEventListener("DOMContentLoaded", () => {
+  showSection('about');
+});
