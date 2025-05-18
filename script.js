2d4893b718fcb0319ec4400f640339c816ec9b6c
flutterbea <loveebea7@gmail.com>
2025-05-18 15:26:32 +0000

    6

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function showSection(id) {
   const sections = document.querySelectorAll('.content');
-  sections.forEach(s => s.classList.remove('active'));
+  sections.forEach(section => {
+    section.classList.remove('active');
+  });
   document.getElementById(id).classList.add('active');
 }
 
-// Открыть первую секцию по умолчанию
 document.addEventListener("DOMContentLoaded", () => {
   showSection('about');
 });
